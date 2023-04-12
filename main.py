a325517244e1aec1532904136a17b09ca55471ce
William Zarzour <will.zarz@gmail.com>
2023-04-11 22:17:43 -0400

    testing output

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ aapl=initialize_ticker("aapl")
 data = get_income_statement(aapl)
 income_statement = standardize_income_statement(data)
 print(income_statement)
+x = average_account_calculation(income_statement)
+print(x)
+y = growth_rate_calculation(income_statement)
+print(y)
+
+
+
 
 '''
 df = pd.DataFrame(list(zip(customers,sales_team_growth,revenue,annual_fixed_costs,annual_variable_costs,wages,marketing_expenses,total_expenses,ebitda,net_income)),
